05e610a90c7711d0795f00d61e6415d8d302bea2
Viral Surani <suraniviral@live.com>
2017-05-20 14:45:47 +0530

    Understanding Control Flow - Measuring Complexity

---
+++ ControlFlow/Understanding Control Flow/ControlFlowDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ControlFlowDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateControlDigit(82712476));
+            Console.ReadKey();
+        }
+
+        static int CalculateControlDigit(int number)
+        {
+            int sum = 0;
+            int pos = 1;
+
+            do
+            {
+                int digit = number % 10;
+
+                int value;
+                if (pos % 2 == 0)
+                    value = digit * 3;
+                else
+                    value = digit;
+
+                sum = sum + value;
+
+                number = number / 10;
+                pos = pos + 1;
+            } while (number > 0);
+
+            int result = sum % 11;
+
+            if (result == 10)
+                result = 1;
+
+            return result;
+        }
+    }
+}
