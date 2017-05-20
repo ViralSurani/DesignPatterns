876dcf2260e926f19e94190c544bf068add86fff
Viral Surani <suraniviral@live.com>
2017-05-20 16:00:55 +0530

    Understanding Control Flow - Optional Method Arguments

--- ControlFlow/Understanding Control Flow/ControlFlowDemo/Program.cs
+++ ControlFlow/Understanding Control Flow/ControlFlowDemo/Program.cs
@@ -7,30 +7,44 @@ namespace ControlFlowDemo
         static void Main(string[] args)
         {
             Console.WriteLine(CalculateControlDigit(82712476));
+
+            Console.WriteLine("{0:C}", GetItemPrice("laptop", 0.15M));
+            Console.WriteLine("{0:C}", GetItemPrice("pen"));
+
             Console.ReadKey();
         }
 
         static int CalculateControlDigit(int number)
         {
-            int sum = 0;            
+            int sum = 0;
             int factor = 3;
-
             do
             {
                 int digit = number % 10;
-
-                sum = sum + (digit*factor);
+                sum = sum + (digit * factor);
                 factor = 4 - factor;
-
-                number = number / 10;                
+                number = number / 10;
             } while (number > 0);
 
             int result = sum % 11;
-
             if (result == 10)
                 result = 1;
-
             return result;
+        }
+
+        private static decimal GetItemPrice(string itemName, decimal relativeDiscount = 0)
+        {
+            if (relativeDiscount < 0 || relativeDiscount >= 1)
+            {
+                throw new ArgumentException("Incorrect discount", "relativeDiscount");
+            }
+
+            if (relativeDiscount > 0)
+            {
+                Console.WriteLine("LOG Discount {0}% applied", 100 * relativeDiscount);
+            }
+
+            return 100.0M * itemName.Length * (1.0M - relativeDiscount);
         }
     }
 }
