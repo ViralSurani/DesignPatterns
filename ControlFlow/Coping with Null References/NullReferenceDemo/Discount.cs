250a498f0d6391ef7f2dc0a47437374eb38eacae
Viral Surani <suraniviral@live.com>
2017-05-27 13:28:27 +0530

    Coping with Null References - Nullable Method Argument

---
+++ ControlFlow/Coping with Null References/NullReferenceDemo/Discount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NullReferenceDemo
+{
+    class Discount
+    {
+        private readonly string _description;
+        private readonly decimal _relativeDiscount;
+
+        public Discount(string description, decimal relativeDiscount)
+        {
+            if (relativeDiscount <= 0 || relativeDiscount >= 1)
+            {
+                throw new ArgumentException("Incorrect discount", "relativeDiscount");
+            }
+            _description = description;
+            _relativeDiscount = relativeDiscount;
+        }
+
+        public decimal Apply(decimal price)
+        {
+            return price * (1.0M - _relativeDiscount);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}% {1}", _relativeDiscount * 100, _description);
+        }
+    }
+}
---
+++ ControlFlow/Coping with Null References/NullReferenceDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullReferenceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Discount discount = new Discount("loyalty discount", 0.15M);
+
+            Console.WriteLine("{0:C}", GetItemPrice("laptop", discount));
+            Console.WriteLine("{0:C}", GetItemPrice("pen", null));
+
+            Console.ReadKey();
+        }
+
+        private static decimal GetItemPrice(string itemName, Discount discount)
+        {
+
+            decimal basePrice = 100.0M * itemName.Length;
+
+            if (discount == null)
+                return basePrice;
+
+            Console.WriteLine("LOG applying {0}", discount);
+
+            return discount.Apply(basePrice);
+        }
+    }
+}
