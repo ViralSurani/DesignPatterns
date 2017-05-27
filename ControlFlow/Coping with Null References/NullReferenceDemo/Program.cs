146bad1813847ab4a8e43669b49b927e59bc4774
Viral Surani <suraniviral@live.com>
2017-05-27 13:31:57 +0530

    Coping with Null References - Nullable Method Argument

--- ControlFlow/Coping with Null References/NullReferenceDemo/Program.cs
+++ ControlFlow/Coping with Null References/NullReferenceDemo/Program.cs
@@ -13,22 +13,25 @@ namespace NullReferenceDemo
             Discount discount = new Discount("loyalty discount", 0.15M);
 
             Console.WriteLine("{0:C}", GetItemPrice("laptop", discount));
-            Console.WriteLine("{0:C}", GetItemPrice("pen", null));
+            Console.WriteLine("{0:C}", GetItemPrice("pen"));
 
             Console.ReadKey();
         }
 
         private static decimal GetItemPrice(string itemName, Discount discount)
         {
-
-            decimal basePrice = 100.0M * itemName.Length;
-
             if (discount == null)
-                return basePrice;
-
+            {
+                throw new ArgumentNullException("discount");
+            }
+            decimal basePrice = GetItemPrice(itemName);
             Console.WriteLine("LOG applying {0}", discount);
-
             return discount.Apply(basePrice);
+        }
+
+        private static decimal GetItemPrice(string itemName)
+        {
+            return 100.0M * itemName.Length;
         }
     }
 }
